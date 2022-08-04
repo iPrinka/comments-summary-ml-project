f1a7668444a58337242928302245310ba178b353
Priyanka-Gangadhar-Palshetkar <notify.priyanka@gmail.com>
2022-08-04 23:22:44 +0530

    Using Youtube API client directly

--- src/comments.py
+++ src/comments.py
@@ -1,4 +1,6 @@
-from config.config import YOUTUBE_API_KEY
+import json
+from googleapiclient.discovery import build
+from config.config import YOUTUBE_API_KEY, API_SERVICE_NAME, API_VERSION
 from pyyoutube import Api
 from pytube import extract
 
@@ -9,8 +11,23 @@ def extract_video_id_from_link(url):
 
 def get_comments(video_link):
     video_id = extract_video_id_from_link(video_link)
-    comments = api.get_comment_threads(video_id=video_id, count=None)
-    return comments
 
-response = get_comments("https://www.youtube.com/watch?v=2jkvr8cHzr4")
-print(response.items[0].to_json())
+    youtube = build(API_SERVICE_NAME, API_VERSION, developerKey=YOUTUBE_API_KEY)
+    request = youtube.commentThreads().list(
+        part="snippet,replies",
+        videoId=video_id,
+        textFormat='plainText',
+        maxResults=100
+    )
+    response = request.execute()
+    response_json = json.dumps(response,indent=2)
+    return(response_json)
+
+
+def main():
+    # test = get_comments("https://www.youtube.com/watch?v=2jkvr8cHzr4&t=5s")
+    test = get_comments("https://www.youtube.com/watch?v=EYncNbM9HMs")
+    print(test)
+
+if __name__ == "__main__":
+    main()
